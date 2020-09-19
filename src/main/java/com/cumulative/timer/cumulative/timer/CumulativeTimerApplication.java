141e114797672bc2250a248071fa91a1cd647f37
Gaurav Massand <gauravm636@gmail.com>
2020-09-19 15:49:58 +0530

    Initial setup

---
+++ src/main/java/com/cumulative/timer/cumulative/timer/CumulativeTimerApplication.java
@@ -0,0 +1,13 @@
+package com.cumulative.timer.cumulative.timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumulativeTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumulativeTimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cumulative/timer/cumulative/timer/CumulativeTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cumulative.timer.cumulative.timer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumulativeTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
